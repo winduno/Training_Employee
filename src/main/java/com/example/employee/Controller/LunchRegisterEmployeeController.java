bab4a2ec83b86543bcfc863152d18449f0b90184
winduno <windsolo294@gmail.com>
2021-03-03 17:22:59 +0700

    create LunchRegisterEmployeeController

---
+++ src/main/java/com/example/employee/Controller/LunchRegisterEmployeeController.java
@@ -0,0 +1,47 @@
+package com.example.employee.Controller;
+
+import com.example.employee.Model.LunchRegisterEmployee;
+import com.example.employee.Service.LunchRegisterEmployee.ILunchRegisterEmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/api/lunchregisteremployee")
+public class LunchRegisterEmployeeController {
+
+    @Autowired
+    private ILunchRegisterEmployeeService lunchRegisterEmployeeService;
+
+    @GetMapping
+    public ResponseEntity<Iterable<LunchRegisterEmployee>> getAllLunchRegisterEmployee(){
+        return new ResponseEntity<>(this.lunchRegisterEmployeeService.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<LunchRegisterEmployee> getLunchRegisterEmployeeById(@PathVariable(name = "id") Long id){
+        Optional<LunchRegisterEmployee> optionalLunchRegisterEmployee = this.lunchRegisterEmployeeService.findById(id);
+        return optionalLunchRegisterEmployee.map(result -> new ResponseEntity<>(result, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<LunchRegisterEmployee> addLunchRegisterEmployee(@RequestBody LunchRegisterEmployee lunchRegisterEmployee){
+        return new ResponseEntity<>(this.lunchRegisterEmployeeService.save(lunchRegisterEmployee), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<LunchRegisterEmployee> deleteLunchRegisterEmployeeById(@PathVariable(name = "id") Long id){
+        Optional<LunchRegisterEmployee> optionalLunchRegisterEmployee = this.lunchRegisterEmployeeService.findById(id);
+        if (optionalLunchRegisterEmployee.isPresent()){
+            this.lunchRegisterEmployeeService.delete(id);
+            return new ResponseEntity<>( HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
